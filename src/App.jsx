119ab94d2fdbfe66755301706e7fc30ede3c47a0
kumarjhamishra <03satyam05@gmail.com>
2025-02-08 13:52:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import ClockHeading from './components/ClockHeading'
+import ClockSlogan from './components/ClockSlogan'
+import CurrentTime from './components/CurrentTime'
+
+function App() {
+  return <center>
+    <ClockHeading></ClockHeading>
+    <ClockSlogan></ClockSlogan>
+    <CurrentTime></CurrentTime>
+  </center>
+}
+
+export default App
---
+++ src/components/CurrentTime.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+let CurrentTime = () =>{
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        console.log("Interval has been setup");
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000); // time updates after every second
+
+        return () => {
+            clearInterval(intervalId);
+            console.log("Cancelled interval");
+        };
+    }, []);
+
+    return <p className="lead">This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+}
+
+export default CurrentTime;
